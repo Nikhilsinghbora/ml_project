25c3eb86c917841386cf0fc9efc0ad818c739fc5
Nikhil <boranikhilsingh@gmail.com>
2023-07-20 11:32:43 +0530

    eda and training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
 
+from src.logger import logging
+
 def error_message_detail(error,error_detail:sys):
    _,_,exc_tb = error_detail.exc_info()
    file_name = exc_tb.tb_frame.f_code.co_filename
@@ -10,12 +12,14 @@ def error_message_detail(error,error_detail:sys):
 
    return error_message
  
-    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init(error_message)
+        super().__init__(error_message)
         self._error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
         return self._error_message
+
+
+
